a31a8f6383262345090d2e7311ec282bb4eb0175
Nada Zirari <zirarinada20@gmail.com>
2025-11-25 16:45:46 +0100

    ajouter methods of get products in serviceimpl

--- src/main/java/com/microtech/smartshop/repository/ProductRepository.java
+++ src/main/java/com/microtech/smartshop/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.microtech.smartshop.repository;
 
 import com.microtech.smartshop.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByDeletedFalse(Pageable pageable);
 
 }
--- src/main/java/com/microtech/smartshop/serviceImpl/ProductServiceImpl.java
+++ src/main/java/com/microtech/smartshop/serviceImpl/ProductServiceImpl.java
@@ -6,11 +6,13 @@ import com.microtech.smartshop.mapper.ProductMapper;
 import com.microtech.smartshop.repository.ProductRepository;
 import com.microtech.smartshop.service.ProductService;
 import org.springframework.data.domain.Page;
+import com.microtech.smartshop.exception.NotFoundException;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProductServiceImpl {
+public class ProductServiceImpl implements ProductService {
 
 
     private final ProductRepository productRepository;
@@ -26,6 +28,38 @@ public class ProductServiceImpl {
         product.setDeleted(false);
         product = productRepository.save(product);
         return productMapper.toDTO(product);
+    }
+    @Override
+    public ProductDTO updateProduct(Long id, ProductCreateDTO dto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Produit non trouvé avec id " + id));
+        productMapper.updateEntityFromDTO(dto, product);
+        product = productRepository.save(product);
+        return productMapper.toDTO(product);
+    }
+
+    @Override
+    public ProductDTO getProductById(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Produit non trouvé avec id " + id));
+        if(product.isDeleted()) {
+            throw new NotFoundException("Produit supprimé");
+        }
+        return productMapper.toDTO(product);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Produit non trouvé avec id " + id));
+        product.setDeleted(true); // soft delete
+        productRepository.save(product);
+    }
+
+    @Override
+    public Page<ProductDTO> getAllProducts(Pageable pageable) {
+        return productRepository.findByDeletedFalse(pageable)
+                .map(productMapper::toDTO);
     }
 
 }
