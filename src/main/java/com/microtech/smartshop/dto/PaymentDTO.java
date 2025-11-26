832a0140c6b5e250b6d8a1529c4364ddb62bb21b
Nada Zirari <zirarinada20@gmail.com>
2025-11-26 11:42:41 +0100

    ajouter paymentdto

---
+++ src/main/java/com/microtech/smartshop/dto/PaymentDTO.java
@@ -0,0 +1,16 @@
+package com.microtech.smartshop.dto;
+
+
+import java.time.LocalDateTime;
+import com.microtech.smartshop.enums.PaymentType;
+import com.microtech.smartshop.enums.PaymentStatus;
+import lombok.Data;
+public class PaymentDTO {
+    private Long id;
+    private Long commandeId;
+    private Double montant;
+    private PaymentType type;
+    private LocalDateTime datePaiement;
+    private LocalDateTime dateEncaissement;
+    private PaymentStatus statut;
+}
