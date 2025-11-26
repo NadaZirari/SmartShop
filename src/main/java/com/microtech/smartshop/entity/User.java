acde235518940c8071283783120825145a770e3a
Nada Zirari <zirarinada20@gmail.com>
2025-11-26 14:59:07 +0100

    ajouter linterface repository avec sa method findbyusername et aussi la classse userservice

--- src/main/java/com/microtech/smartshop/entity/User.java
+++ src/main/java/com/microtech/smartshop/entity/User.java
@@ -1,13 +1,7 @@
 package com.microtech.smartshop.entity;
 
 import com.microtech.smartshop.enums.UserRole;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -16,12 +10,24 @@ import lombok.*;
 @Data
 public class User {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
     private UserRole role;
+
+
+
+
+
+
+
 }
---
+++ src/main/java/com/microtech/smartshop/serviceImpl/UserService.java
@@ -0,0 +1,19 @@
+package com.microtech.smartshop.serviceImpl;
+import com.microtech.smartshop.entity.User;
+import com.microtech.smartshop.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+
+
+}
